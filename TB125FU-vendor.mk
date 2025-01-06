9de2dacd083cc60d9bf4e16fe8a4558b41c97a4b
Neco Arc <necoarc001@icloud.com>
2025-01-06 21:38:36 +0100

    Update TB125FU-vendor.mk

--- TB125FU-vendor.mk
+++ TB125FU-vendor.mk
@@ -1603,7 +1603,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1.xml \
     android.hardware.neuralnetworks@1.3-service-mtk-gpu.xml \
     android.hardware.usb@1.2-service-mediatekv2.xml \
-    android.hardware.wifi.hostapd.xml \
     android.hardware.wifi@1.0-service.xml \
     gnss@2.1-service.xml \
     manifest_android.hardware.drm@1.4-service.clearkey.xml \
